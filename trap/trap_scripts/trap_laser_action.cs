5f0f12a987a698ade78ba24faf66c2c7a0779ba7
Min Hye Won <mwon0326@gmail.com>
2020-02-13 00:04:03 +0900

    파티클 프리팹, 함정 프리팹 추가

---
+++ trap/trap_scripts/trap_laser_action.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_laser_action : MonoBehaviour
+{
+    public ParticleSystem trap_particle; // 재생할 함정 파티클
+    public float trap_playTime; // 함정이 발동되는 시간
+    public float trap_stopTime; // 함정이 멈춰있는 시간
+
+    private Collider trap_collider;
+    private bool is_trap = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trap_collider = GetComponent<Collider>();
+        StartCoroutine(TrapEffect());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (is_trap)
+            Debug.Log("Dead");
+    }
+
+    private IEnumerator TrapEffect()
+    {
+        while (true)
+        {
+            is_trap = true;
+            trap_particle.Play();
+            yield return new WaitForSeconds(trap_playTime);
+
+            is_trap = false;
+            trap_particle.Stop();
+            trap_particle.Clear();
+            yield return new WaitForSeconds(trap_stopTime);
+        }
+    }
+}
---
+++ trap/trap_scripts/trap_niddle_action.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_niddle_action : MonoBehaviour
+{
+    public float max_height; // 최고 도달점
+    public float min_height; // 최저 도달점
+    public float velocity; // 움직이는 속도
+    public GameObject niddle;
+    
+    private bool isUP = true; // 올라가고 있는지 내려가고 있는지
+    private bool isWait = false;
+    private Collider niddle_collider;
+    private float start_velocity;
+    private Transform niddle_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        niddle_collider = GetComponent<Collider>();
+        niddle_position = niddle.transform;
+        start_velocity = velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUP)
+        {
+            niddle_position.position = Vector3.Lerp(niddle_position.position, 
+            max_height * Vector3.up, velocity * Time.deltaTime);
+
+            float distance = max_height - niddle_position.position.y;
+
+            if (distance < float.Epsilon + 0.01f)
+            {
+                isUP = false;
+            }
+        }
+        else
+        {
+            niddle_position.position = Vector3.MoveTowards(niddle_position.position,
+                min_height * Vector3.up, velocity * Time.deltaTime);
+
+            float distance = niddle_position.position.y - min_height;
+
+            if (distance < float.Epsilon + 0.01f)
+            {
+                if (!isWait)
+                    StartCoroutine(WaitTime());
+            }
+        }
+    }
+
+    IEnumerator WaitTime()
+    {
+        isWait = true;
+        yield return new WaitForSeconds(5f);
+        isUP = true;
+        isWait = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isWait)
+            Debug.Log("Dead");
+    }
+}
+
